d71c920f5e94ab0bdfdf2dc868e3b3dd5fa73956
Oskar Hahn <mail@oshahn.de>
2024-10-17 18:17:30 +0200

    Fix typo

--- web/template/login_templ.go
+++ web/template/login_templ.go
@@ -286,7 +286,7 @@ func register(err string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var19 := `angiebst. Nachdem du alles ausgefüllt hast, kannst du deinen`
+		templ_7745c5c3_Var19 := `angibst. Nachdem du alles ausgefüllt hast, kannst du deinen`
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var19)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
